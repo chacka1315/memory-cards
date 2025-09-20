0dae990fd6dd46d5f378d0fb9c0cf588c99a778d
chacka1315 <chacka131@gmail.com>
2025-09-20 13:43:34 +0000

    I write the DashBoard components. I use the useState hook in App.js to pass scores in props to the DashBoard.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import './App.css';
+import DashBoard from './components/DashBoard';
 
 function App() {
-  return <></>;
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  return (
+    <>
+      <DashBoard score={score} bestScore={bestScore} />
+    </>
+  );
 }
 
 export default App;
